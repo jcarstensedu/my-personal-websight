ec141b25b054f031276a837e2e5386262a57222c
johnathan carstens <jcarstensedu@gmail.com>
2023-03-17 20:02:26 -0600

    added base format. Linked css with html. linked JS with html

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function addingEventListener() {
+}
+const input = document.getElementById('button');
 
-document.querySelector("body").appendChild(h2);
+function clickAlert() {
+    alert('SOOOO SICK');
+}
+
+input.addEventListener('click', clickAlert);
